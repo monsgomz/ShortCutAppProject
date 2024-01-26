68ae55c0af5f0b40fce6ca308e2fb5d7570be3c9
M Montserrat Gomez <gome0145@algonquinlive.com>
2024-01-26 15:30:01 -0500

    Initial Commit

---
+++ NoteAppProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NoteAppProject
+//
+//  Created by Montserrat Gomez on 2024-01-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NoteAppProject/NoteAppProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteAppProjectApp.swift
+//  NoteAppProject
+//
+//  Created by Montserrat Gomez on 2024-01-26.
+//
+
+import SwiftUI
+
+@main
+struct NoteAppProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
